4cad05c5928e91fddc79ca2b4c5e5b0fb081a6d0
amsteradams <adam.k92@hotmail.fr>
2022-05-13 11:48:35 +0200

    ajout migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
 var SellingFactory = artifacts.require("./SellingFactory.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(SellingFactory);
+var Storage = artifacts.require("./Storage.sol");
+var NftFactory = artifacts.require("./NftFactory.sol");
+module.exports =async function(deployer) {
+  try {
+    await deployer.deploy(SellingFactory);
+    const storage = await deployer.deploy(Storage);
+    await deployer.deploy(NftFactory, storage.address);  
+  } catch (error) {
+    console.log(error);
+  }
+  
 };
--- migrations/2_deploy.js
+++
@@ -1,5 +0,0 @@
-var mockERC721 = artifacts.require("./mockERC721.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(mockERC721);
-};
